26a20467bbe5632e69af590d017c4213c7203415
Adrian <arojasgomez.93@gmail.com>
2025-10-11 07:25:02 -0300

    Initial csv works

---
+++ Model/DocHeader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class DocHeader
+    {
+        public string Environment { get; set; }
+        public DateTime Date { get; set; }
+        public string Site { get; set; }
+        public string Credentials { get; set; }
+        public string Tester { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
