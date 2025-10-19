c64d79d3a5043cda2760b74a5262f24426235a1c
Adrian <arojasgomez.93@gmail.com>
2025-10-19 11:19:35 -0300

    docx Completo y reuso

--- Model/DocxHeader.cs
+++ Model/DocxHeader.cs
@@ -6,14 +6,13 @@ using System.Threading.Tasks;
 
 namespace Data
 {
-    public class DocHeader
+    public class DocxHeader
     {
         public string Environment { get; set; }
-        public DateTime Date { get; set; }
+        public string Date { get; set; }
         public string Site { get; set; }
         public string Credentials { get; set; }
-        public string Tester { get; set; }
-        public string Notes { get; set; }
-
+        public string TesterName { get; set; }
+        public string JiraKey { get; set; }
     }
 }
